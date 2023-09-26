4938ae6d031bbe3934b5a8425321b01bf36fbcd0
Pedro Pizarro <125596897+PedroArthurPizarro@users.noreply.github.com>
2023-09-26 14:23:54 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const http = require('http');
+const socketIo = require('socket.io');
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/Index.html');
+});
+
+io.on('connection', (socket) => {
+    console.log('Um usuário se conectou');
+
+    socket.on('chat message', (msg) => {
+        io.emit('chat message', msg);
+    });
+
+    socket.on('disconnect', () => {
+        console.log('Usuário desconectado');
+    });
+});
+
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => {
+    console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
